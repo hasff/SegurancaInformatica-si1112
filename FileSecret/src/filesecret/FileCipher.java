cc62a025f2d54d926c801a89dc69a1f19b963ccd
hiddenbyte <mehulira@gmail.com>
2011-10-05 22:35:09 +0100

    Cipher usage correction

--- FileSecret/src/filesecret/FileCipher.java
+++ FileSecret/src/filesecret/FileCipher.java
@@ -1,13 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package filesecret;
 
-import java.awt.RenderingHints.Key;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.KeyStore;
@@ -20,59 +16,62 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 
-/**
- *
- * @author Hugo
- */
 public class FileCipher {
-
+    private static final int BUFFER_SIZE = 1024;
+    private static final String SYMMETRIC_CRYPTOGRAPHIC_ALGORITHM = "DES";
+    
     private final X509Certificate _X509Cerfificate;
     private final KeyStore _KeyStore;
-    //private final String 
     
     public FileCipher(
             X509Certificate x509Certificate, 
             KeyStore keyStore,
             String pathCerts)
     {
-        if (x509Certificate == null) throw new IllegalArgumentException("x509Certificate: can not be null!");
-        if (keyStore == null) throw new IllegalArgumentException("KeyStore: can not be null!");
+        if (x509Certificate == null)
+            throw new IllegalArgumentException("x509Certificate: can not be null!");
+        if (keyStore == null)
+            throw new IllegalArgumentException("KeyStore: can not be null!");
         
         this._X509Cerfificate = x509Certificate;
         this._KeyStore = keyStore;
-       
+        
+        //TODO: validate 'x509Certificate' certificate
     }
     
     // Cipher
-    public FileCipherResult doCipher(String file_path) throws NoSuchAlgorithmException, NoSuchPaddingException, FileNotFoundException, IOException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+    public FileCipherResult doCipher(String file_path) throws NoSuchAlgorithmException, 
+            NoSuchPaddingException, FileNotFoundException, IOException, 
+            InvalidKeyException, IllegalBlockSizeException, BadPaddingException
     {
         FileCipherResult result = null; 
-        String cryptographicAlgorithm = "DES";
-        int blockSize = 1024;
-
+        
         // Generate Public Simetric Key.
-        KeyGenerator keyGen = KeyGenerator.getInstance(cryptographicAlgorithm);
+        KeyGenerator keyGen = KeyGenerator.getInstance(SYMMETRIC_CRYPTOGRAPHIC_ALGORITHM);
         SecretKey ks = keyGen.generateKey();
         
-        // Get file content in byte[].
+        //Create 'file to encrypt' stream reader
         File file = new File(file_path);
         FileInputStream fileInputStream = new FileInputStream(file);
         
-        // Encrypt algorithm.
-        Cipher c = Cipher.getInstance(cryptographicAlgorithm);
+        //Create 'file to encrypt' cryptogram stream
+        FileOutputStream cryptogramOutStream = 
+                new FileOutputStream(String.format("%s.secret", file_path));
+        
+        //Create and init cipher algorithm
+        Cipher c = Cipher.getInstance(SYMMETRIC_CRYPTOGRAPHIC_ALGORITHM);
         c.init(Cipher.ENCRYPT_MODE, ks);
         
-        byte fileContentByteArr[] = new byte[blockSize];
+        //Encrypt!
+        byte fileContentByteArr[] = new byte[BUFFER_SIZE];
+        int readedBytes;
         
-        while (fileInputStream.read(fileContentByteArr) != -1)
-        {
-            c.update(fileContentByteArr);
-        }
+        while ((readedBytes = fileInputStream.read(fileContentByteArr)) != -1)
+            cryptogramOutStream.write(c.update(fileContentByteArr,0,readedBytes));
+        cryptogramOutStream.write(c.doFinal());
         
-        c.doFinal();
+        //TODO: create metadata file
         
         return result;
     }
-    
-    
 }
--- FileSecret/src/filesecret/FileCipherResult.java
+++ FileSecret/src/filesecret/FileCipherResult.java
@@ -1,14 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package filesecret;
 
-/**
- *
- * @author Hugo
- */
-class FileCipherResult {
+public class FileCipherResult {
     
     public FileCipherResult()
     {
